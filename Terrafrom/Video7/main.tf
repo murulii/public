90b1584ab8194ea88d72457351906e7de0739935
murulii <muruli800@gmail.com>
2024-04-24 13:00:22 +0530

    s

--- Terrafrom/Video7/main.tf
+++
@@ -1,8 +0,0 @@
-module "aws_ec2" {
-
-    source = "./modules/aws_ec2"
-
-    aws_ami = var.aws_ami
-    instance_type = var.instance_type
-  
-}
--- Terrafrom/Video7/stage.tfvars
+++
@@ -1,2 +0,0 @@
-aws_ami = "ami-0fa377108253bf620"
-instance_type = "t2.nano"
