730bd10968851dbb26dd103ab9db97ff778ebd9c
murulii <muruli800@gmail.com>
2024-04-29 13:40:29 +0530

    Create 4_cert_create.sh

---
+++ Installation/KubeadmHA_sh_files/4_cert_create.sh
@@ -0,0 +1,9 @@
+cd openssl
+openssl genrsa -out ca-key.pem 2048
+openssl req -new -key ca-key.pem -out ca-csr.pem -subj "/CN=etcd cluster"
+openssl x509 -req -in ca-csr.pem -out ca.pem -days 3650 -signkey ca-key.pem -sha256
+openssl genrsa -out etcd-key.pem 2048
+openssl req -new -key etcd-key.pem -out etcd-csr.pem -subj "/CN=etcd"
+
+echo subjectAltName = DNS:localhost,IP:192.168.1.2,IP:192.168.1.3,IP:192.168.1.4,IP:127.0.0.1 > extfile.cnf
+openssl x509 -req -in etcd-csr.pem -CA ca.pem -CAkey ca-key.pem -CAcreateserial -days 3650 -out etcd.pem -sha256 -extfile extfile.cnf
