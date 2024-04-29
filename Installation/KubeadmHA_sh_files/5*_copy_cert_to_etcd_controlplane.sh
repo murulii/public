f063946dee29fa7f831ea8ca30acd10e798a4232
murulii <muruli800@gmail.com>
2024-04-29 13:41:46 +0530

    Create 5*_copy_cert_to_etcd_controlplane.sh

---
+++ Installation/KubeadmHA_sh_files/5*_copy_cert_to_etcd_controlplane.sh
@@ -0,0 +1,12 @@
+
+scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@74.220.16.178:/var/lib/etcd
+scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@74.220.19.12:/var/lib/etcd
+scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@74.220.21.101:/var/lib/etcd
+
+
+
+scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@74.220.23.131:/etcd/kubernetes/pki/etcd/
+scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@74.220.19.191:/etcd/kubernetes/pki/etcd/
+scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@74.220.22.42:/etcd/kubernetes/pki/etcd/
+
+
