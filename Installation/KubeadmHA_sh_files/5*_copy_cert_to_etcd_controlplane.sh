a00cc4a67bd32df0155106db72d43773efb3c967
murulii <muruli800@gmail.com>
2024-04-29 17:01:37 +0530

    Update 5*_copy_cert_to_etcd_controlplane.sh

--- Installation/KubeadmHA_sh_files/5*_copy_cert_to_etcd_controlplane.sh
+++ Installation/KubeadmHA_sh_files/5*_copy_cert_to_etcd_controlplane.sh
@@ -1,12 +1,12 @@
 
-scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@74.220.16.178:/var/lib/etcd
-scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@74.220.19.12:/var/lib/etcd
-scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@74.220.21.101:/var/lib/etcd
+scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@${etcd-0}:/var/lib/etcd
+scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@${etcd-1}:/var/lib/etcd
+scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@${etcd-2}:/var/lib/etcd
 
 
 
-scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@74.220.23.131:/etcd/kubernetes/pki/etcd/
-scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@74.220.19.191:/etcd/kubernetes/pki/etcd/
-scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@74.220.22.42:/etcd/kubernetes/pki/etcd/
+scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@${Controlplane_Private_IP_0}:/etcd/kubernetes/pki/etcd/
+scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@${Controlplane_Private_IP_1}:/etcd/kubernetes/pki/etcd/
+scp -i ksctl-key ca.pem etcd.pem etcd-key.pem root@${Controlplane_Private_IP_2}:/etcd/kubernetes/pki/etcd/
 
 
